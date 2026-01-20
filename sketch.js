e1078f664d45cf2eadefdc6252691d68a021fa55
lenn <lennyzengerle@gmail.com>
2026-01-20 10:44:17 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+drawTarget(width/2 height/2);
+}
+
+function drawTarget (x, y);{
+ellipse (50, 50, 80, 80);
+}
