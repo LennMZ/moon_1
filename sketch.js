cb26ddbdfbc272c9580ffaa26932f38b9a470d36
lenn <lennyzengerle@gmail.com>
2026-01-20 10:55:50 -0800

    Drew Target

--- sketch.js
+++ sketch.js
@@ -3,10 +3,11 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(220, 0, 0);
 drawTarget(width/2 height/2);
 }
 
 function drawTarget (x, y);{
 ellipse (50, 50, 80, 80);
+elli[se ( 255, 255, 40, 40);]
 }
