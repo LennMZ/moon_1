69782916ba5f69dd16db32f565b3d4d32c850e19
lenn <lennyzengerle@gmail.com>
2026-01-20 11:06:23 -0800

    Test Push

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
+let targetAn =width/2, height/2;
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
+  targetAn++;
+  tagetrAn = targetAnn % 25;
   background(220, 0, 0);
-drawTarget(width/2 height/2);
+
+  drawTarget(targetAn);
 }
 
-function drawTarget (x, y);{
+function drawTarget (x, y){
 ellipse (50, 50, 80, 80);
-elli[se ( 255, 255, 40, 40);]
+ellipse ( 255, 255, 60, 60);
+ellipse (50, 50, 40, 40);
+ellipse (255, 255, 20, 20);
 }
